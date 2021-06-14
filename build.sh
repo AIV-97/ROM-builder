cf8304cf8d9e85cf5c3bcbb7a113f8b94ef6236b
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-06-14 13:24:59 +0530

    Test build

--- build.sh
+++ build.sh
@@ -14,10 +14,10 @@ git clone "$LOCAL_MANIFEST" --depth 1 -b "$MANIFEST_BRANCH" .repo/local_manifest
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 30 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# patches
+# Patches
 
 cd external/selinux
-wget https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
+curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
 patch -p1 < *.patch
 cd ../..
 
@@ -27,12 +27,12 @@ cd ../..
 #cd ../..
 
 #cd frameworks/opt/net/wifi
-#wget https://github.com/PixelExperience/frameworks_opt_net_wifi/commit/3bd2c14fbda9c079a4dc39ff4601ba54da589609.patch
+#curl -LO https://github.com/PixelExperience/frameworks_opt_net_wifi/commit/3bd2c14fbda9c079a4dc39ff4601ba54da589609.patch
 #patch -p1 < *.patch
 #cd ../../../..
 
 cd frameworks/opt/net/ims
-wget https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch
+curl -LO https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch
 patch -p1 < *.patch
 cd ../../../..
 
