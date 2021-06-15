485266378334751774c938fc489f9f66f6896c41
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-06-15 13:47:06 +0530

    Added metalava some stuff

--- build.sh
+++ build.sh
@@ -43,6 +43,10 @@ cd /tmp/rom
 . build/envsetup.sh
 lunch lineage_RMX1941-userdebug
 
+export SKIP_API_CHECKS=true
+export SKIP_ABI_CHECKS=true
+export _JAVA_OPTIONS=-Xmx16g
+
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
@@ -51,13 +55,19 @@ ccache -M 20G
 ccache -o compression=true
 ccache -z
 
-# use first three lines one time while generating ccache and 2nd time at time of final build
-#make api-stubs-docs
-#make system-api-stubs-docs
-#make test-api-stubs-docs
-make bacon -j$(nproc --all) &
-sleep 90m
-kill %1 || echo "Build already failed or completed"
+
+# metalava
+make_metalava(){
+        mka api-stubs-docs
+        mka system-api-stubs-docs
+        mka test-api-stubs-docs
+}
+
+make_metalava
+
+mka bacon -j$(nproc --all)
+#sleep 90m
+#kill %1 || echo "Build already failed or completed"
 ccache -s
 
 # upload
